99cb3c35a60d1a329502b9d36640a82e47a9a58d
enzomâncio <enzo07.vaz@gmail.com>
2024-05-02 19:19:07 -0300

    Aula 02 JS

---
+++ JS/02_Console_Variáveis_Operadores_JS/JavaScript.js
@@ -0,0 +1,7 @@
+
+const nome = ' Enzo Mancio Vaz '
+const altura = ' 1.80m '
+const peso = ' 64Kg '
+const IMC = (peso/altura**2)
+
+console.log('Meu nome é' + nome + 'possuo uma altura de' +altura+ 'e peso' + peso + ', meu IMC atual é de' + IMC +'.')
