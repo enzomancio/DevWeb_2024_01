5beeeefa3133c7906cf15e1f6c7b30a081fe3ae6
enzomâncio <enzo07.vaz@gmail.com>
2024-05-02 19:40:37 -0300

    exercício moodle console JS

--- JS/02_Console_Variáveis_Operadores_JS/JavaScript.js
+++ JS/02_Console_Variáveis_Operadores_JS/JavaScript.js
@@ -1,7 +1,7 @@
 
-const nome = ' Enzo Mancio Vaz '
-const altura = ' 1.80m '
-const peso = ' 64Kg '
+const nome =  ' Enzo Mancio Vaz '  
+const altura =  1.80 
+const peso =  64 
 const IMC = (peso/altura**2)
 
-console.log('Meu nome é' + nome + 'possuo uma altura de' +altura+ 'e peso' + peso + ', meu IMC atual é de' + IMC +'.')
+console.log('Meu nome é' + nome + 'possuo uma altura de ' +altura+ ' e peso ' + peso + ', meu IMC atual é de ' + IMC +'.')
