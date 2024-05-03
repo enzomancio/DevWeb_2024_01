cd7261816c8b5fa6eaeb5384d63536b3aa3dcf32
enzomâncio <enzo07.vaz@gmail.com>
2024-05-02 21:34:30 -0300

    aula 03 JS

---
+++ JS/03_Strings_JS/string.js
@@ -0,0 +1,16 @@
+let pera = 'Pera'
+let banana = 'Banana'
+let morango = '   Morango   '
+let laranja = 'Laranja'
+let uva= 'Uva'
+
+console.log(pera.length)
+console.log(banana.substring(0, 3))
+console.log(morango.trim())
+
+console.log(laranja)
+let Laranja = laranja.replace('Laranja', 'laranja')
+console.log(Laranja)
+console.log(uva.toLowerCase());
+
+console.log(`Minhas cinco frutas favoritas são: ${pera}, ${banana}, ${morango}, ${laranja}e ${uva} `)
