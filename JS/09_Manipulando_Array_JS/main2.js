04b9104693b75ca514a22c7e44b0aec70591e192
enzomâncio <enzo07.vaz@gmail.com>
2024-05-20 19:10:57 -0300

    .

---
+++ JS/09_Manipulando_Array_JS/main2.js
@@ -0,0 +1,9 @@
+const funcionários = [
+    {nome: 'Wally', salário: '4800'},
+    {nome: 'Luis', salário: '3500'},
+    {nome: 'Marcelo', salário: '3700'},
+    {nome: 'Bárbara', salário: '5000'},
+    {nome: 'Enzo', salário: '5200'}
+
+]
+funcionários.forEach
