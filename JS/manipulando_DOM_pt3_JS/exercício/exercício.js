6cd983b1c3496b230bf087968b0f8c9a1620daec
enzomâncio <enzo07.vaz@gmail.com>
2024-05-21 21:18:22 -0300

    aula de DOM pt 3 e exercício

---
+++ JS/manipulando_DOM_pt3_JS/exercício/exercício.js
@@ -0,0 +1,10 @@
+
+const Clique = () => {
+    document.querySelector('.my-element').style.backgroundColor = '#CEFBBA';
+    };
+
+const clique = () => {
+    let myItem = document.querySelectorAll('.item');
+    myItem.forEach((item) => (item.style.backgroundColor = '#E7BAFB'));
+
+};
